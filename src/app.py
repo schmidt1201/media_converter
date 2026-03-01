31e3737683017577a26a2262e14337bc016ec56d
Schmidt1999 <matheusschmidtf@protonmail.com>
2026-03-01 16:45:52 -0300

    Added dropdown selector 01/03/2026

--- src/app.py
+++ src/app.py
@@ -17,6 +17,19 @@ class App:
         self.drop_label.drop_target_register(DND_FILES) # registers label as a place where files can be dropped 
         self.drop_label.dnd_bind("<<Drop>>", self.handle_drop) # when file is dropped, runs handle_drop function
 
+        # Variable to store selected format
+        self.selected_format = tk.StringVar(self.root)
+        self.selected_format.set("mp3")  # default value
+
+        #create dropdown menu
+        self.format_menu = tk.OptionMenu(
+            self.root,
+            self.selected_format,
+            "mp3", "wav", "ogg"
+        )
+        self.format_menu.pack(pady=10)
+
+
     def handle_drop(self, event):
         self.file_path = event.data # stores file path 
         self.drop_label.config(text=f"File: {self.file_path}") # changes drop label text to show file name
@@ -24,4 +37,4 @@ class App:
 if __name__ == "__main__":
     root = TkinterDnD.Tk()
     app = App(root)
-    root.mainloop()
+    root.mainloop()  
